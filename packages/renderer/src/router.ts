d8d377ede8d13dedbd5155ab5d89f9cc99539177
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-11-22 21:16:22 +0100

    refactor: remove unused vue components

--- packages/renderer/src/router.ts
+++ packages/renderer/src/router.ts
@@ -1,9 +1,8 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
-import Home from '/@/components/Home.vue';
+import HomePage from '/@/components/HomePage.vue';
 
 const routes = [
-  {path: '/', name: 'Home', component: Home},
-  {path: '/about', name: 'About', component: () => import('/@/components/About.vue')}, // Lazy load route component
+  {path: '/', name: 'Home', component: HomePage},
 ];
 
 export default createRouter({
